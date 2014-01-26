8ec8288162ce2b05306c14c9d832bc8c81f9773d
James Webber <bunkerprivate@gmail.com>
2014-01-26 22:57:58 +0000

    Command pattern with server start command.

--- craftrun/cli.py
+++ craftrun/cli.py
@@ -1,16 +1,31 @@
 import argparse, sys
+from craftrun.command import StartCommand
 
 class CraftRunCli(object):
+    """
+    Creates commands and runs them with apropriate settings.
+    """
+    def __init__(self):
+        self.commands = [
+            StartCommand(),
+        ]
+
     def run(self):
         parser = self.get_parser()
         parsed = parser.parse_args()
-        return 0
+
+        selected = next((command for command in self.commands
+                         if command.name == parsed.command))
+
+        return selected.run(parsed)
 
     def get_parser(self):
         parser = argparse.ArgumentParser()
-        subparsers = parser.add_subparsers(help='sub-command help')
+        subparsers = parser.add_subparsers(help='sub-command help', dest="command")
 
-        start = subparsers.add_parser('start')
+        for command in self.commands:
+            subparser = subparsers.add_parser(command.name)
+            command.configure_cli(subparser)
 
         return parser
 
---
+++ craftrun/command.py
@@ -0,0 +1,9 @@
+class StartCommand(object):
+    """Start the server."""
+    name = "start"
+
+    def configure_cli(self, parser):
+        pass
+
+    def run(self, settings):
+        pass
