edb2a7bbf835acff3bdb7b9a82786a239fd226f4
James Webber <bunkerprivate@gmail.com>
2014-01-27 21:04:36 +0000

    Start command uses config file to start proper server.

--- craftrun/cli.py
+++ craftrun/cli.py
@@ -1,5 +1,38 @@
-import argparse, sys
-from craftrun.command import StartCommand
+import argparse, sys, yaml, os, logging
+from craftrun import command
+
+class Settings(object):
+    """Cli and config file settings."""
+    def __init__(self, cli):
+        self.cli = cli
+
+        with open(cli.config, 'r') as io:
+            self.config = yaml.load(io.read())
+
+    @property
+    def base_dir(self):
+        return self._absolute_path(self.config['base_dir'])
+
+    @property
+    def server_name(self):
+        return self.config['server_name']
+
+    @property
+    def java_bin(self):
+        return self.config.get('java_bin', 'java')
+
+    @property
+    def server_jar(self):
+        return self.config['server_jar']
+
+    @property
+    def java_args(self):
+        default_args = ['-Xmx2G', '-XX:MaxPermSize=256M']
+        return self.config.get('java_args', default_args)
+
+    def _absolute_path(self, path):
+        config_dir = os.path.dirname(self.cli.config)
+        return os.path.join(config_dir, path)
 
 class CraftRunCli(object):
     """
@@ -12,12 +45,17 @@ class CraftRunCli(object):
 
     def run(self):
         parser = self.get_parser()
+        parser.add_argument("-c", "--config", dest="config")
         parsed = parser.parse_args()
+
+        logging.basicConfig(level=logging.INFO)
 
         selected = next((command for command in self.commands
                          if command.name == parsed.command))
 
-        return selected.run(parsed)
+        config = Settings(cli=parsed)
+
+        return selected(config).run()
 
     def get_parser(self):
         parser = argparse.ArgumentParser()
--- craftrun/command.py
+++ craftrun/command.py
@@ -1,9 +1,56 @@
+import os, contextlib, subprocess, logging, time
+from craftrun.screen import ScreenSession
+
 class StartCommand(object):
     """Start the server."""
     name = "start"
 
-    def configure_cli(self, parser):
+    class Error(Exception):
         pass
 
-    def run(self, settings):
+    @classmethod
+    def configure_cli(cls, parser):
         pass
+
+    def __init__(self, settings):
+        self.settings = settings
+        self.screen = ScreenSession(name=settings.server_name)
+
+    def run(self):
+        try:
+            with self.in_dir(self.settings.base_dir):
+                self._launch_server()
+            return 0
+        except self.Error as ex:
+            logging.error(ex)
+            return 1
+
+    def _get_command(self, settings):
+        java = [settings.java_bin] + settings.java_args
+        server = ["-jar", settings.server_jar, 'nogui']
+        return java + server
+
+    def _launch_server(self):
+        if self.screen.is_running():
+            why = "session {0!r} already running"
+            raise self.Error(why.format(self.screen.name))
+
+        command = self._get_command(self.settings)
+        self.screen.start(command)
+
+        time.sleep(1)
+
+        if not self.screen.is_running():
+            raise self.Error("server did not start")
+
+        logging.info("probably running")
+
+    @contextlib.contextmanager
+    def in_dir(self, path):
+        restore_to = os.getcwd()
+        try:
+            logging.info("chdir {0!r}".format(path))
+            os.chdir(path)
+            yield
+        finally:
+            os.chdir(restore_to)
